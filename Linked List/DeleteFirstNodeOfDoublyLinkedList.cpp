f9cb56d5f210eac06e9694f2f226d32d9d48b6c7
MousamSingh <81910954+MousamSingh2612@users.noreply.github.com>
2022-08-30 20:06:12 +0530

    ..

--- Linked List/DeleteFirstNodeOfDoublyLinkedList.cpp
+++ Linked List/DeleteFirstNodeOfDoublyLinkedList.cpp
@@ -25,3 +25,19 @@ Node* delFirstNode(Node* head)
 	delete temp;
 	return head;
 }
+int main()
+{
+	Node* head = new Node(10);
+	Node* temp1 = new Node(20);
+	Node* temp2 = new Node(30);
+
+	head->next = temp1;
+	temp1->prev = head;
+	temp1->next = temp2;
+	temp2->prev = temp1;
+
+	head = delFirstNode(head);   //Always store whatever is being returned by Function in a variable.
+	for (Node* tempo = head; tempo != NULL; tempo = tempo->next)
+		cout << tempo->data << " ";
+	return 0;
+}
